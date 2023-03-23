5c280ef80c9753996f17a16ac703a83125c0ec43
Thiago Felipe <thiago.felipe@tutanota.com>
2023-03-23 01:31:59 -0400

    [Feature] Fazendo prototipo de como enviar um email

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/thiago-felipe-99/mail-consumer
 go 1.20
 
 require (
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/wneessen/go-mail v0.3.9 // indirect
+	github.com/joho/godotenv v1.5.1
+	github.com/wneessen/go-mail v0.3.9
 )
--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
+	"github.com/wneessen/go-mail"
 )
 
 type remetente struct {
-	email, senha, host, porta string
+	nome, email, senha, host string
+	porta              int
 }
 
 type configuracoes struct {
@@ -17,17 +20,62 @@ type configuracoes struct {
 
 func pegarConfiguracoes() (*configuracoes, error) {
 	err := godotenv.Load()
+	if err != nil {
+		return nil, err
+	}
+
+	porta, err := strconv.Atoi(os.Getenv("SMTP_PORT"))
+	if err != nil {
+		return nil, err
+	}
 
 	config := &configuracoes{
 		remetente: remetente{
+      nome: os.Getenv("SMTP_USERNAME"),
 			email: os.Getenv("SMTP_USER"),
 			senha: os.Getenv("SMTP_PASSWORD"),
 			host:  os.Getenv("SMTP_HOST"),
-			porta: os.Getenv("SMTP_PORT"),
+			porta: porta,
 		},
 	}
 
-	return config, err
+	return config, nil
+}
+
+type email struct {
+	destinatario, descricao, mensagem string
+	caminhoAnexos                     []string
+}
+
+func enviarEmail(remetente remetente, email email) error {
+	opcoesCliente := []mail.Option{
+		mail.WithPort(remetente.porta),
+		mail.WithSMTPAuth(mail.SMTPAuthPlain),
+		mail.WithUsername(remetente.email),
+		mail.WithPassword(remetente.senha),
+		mail.WithTLSPolicy(mail.TLSMandatory),
+	}
+
+	cliente, err := mail.NewClient(remetente.host, opcoesCliente...)
+	if err != nil {
+		return err
+	}
+
+	mensagem := mail.NewMsg()
+	err = mensagem.EnvelopeFromFormat(remetente.nome, remetente.email)
+	if err != nil {
+		return err
+	}
+
+	err = mensagem.AddTo(email.destinatario)
+	if err != nil {
+		return err
+	}
+
+	mensagem.Subject(email.descricao)
+	mensagem.SetBodyString(mail.TypeTextPlain, email.mensagem)
+
+	return cliente.DialAndSend(mensagem)
 }
 
 func main() {
@@ -36,5 +84,17 @@ func main() {
 		log.Fatalf("Erro ao ler as configurações: %v", err)
 	}
 
-	log.Println(config)
+	email := email{
+		destinatario:  os.Getenv("EMAIL_TEST"),
+		descricao:     "Testando o serviço de email",
+		mensagem:      "Uma mensagem bem bonita",
+		caminhoAnexos: []string{},
+	}
+
+	err = enviarEmail(config.remetente, email)
+	if err != nil {
+		log.Fatalf("Erro ao enviar o email: %v", err)
+	}
+
+  log.Println("Email enviado com sucesso")
 }
