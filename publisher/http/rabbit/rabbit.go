a72f84d32751734d10f9f219e5ad770155922f61
Thiago Felipe <thiago.felipe@tutanota.com>
2023-04-22 19:40:41 -0300

    Implementing that when sending a message wait for confirmation from RabbitMQ

--- publisher/http/rabbit/rabbit.go
+++ publisher/http/rabbit/rabbit.go
@@ -3,9 +3,18 @@ package rabbit
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"time"
 
 	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+var (
+	ErrConnectionClose  = errors.New("closed connection with RabbitMQ")
+	ErrEncondingMessage = errors.New("error enconding message")
+	ErrSendingMessage   = errors.New("error sending message")
+	ErrMaxRetries       = errors.New("error max retries")
 )
 
 type Config struct {
@@ -17,11 +26,21 @@ type Config struct {
 }
 
 type Rabbit struct {
-	connection *amqp.Connection
-	channel    *amqp.Channel
+	done              chan bool
+	close             bool
+	maxPublishRetries int
+	connection        *amqp.Connection
+	channel           *amqp.Channel
+	notifyClose       chan amqp.Error
 }
 
 func (rabbit *Rabbit) Close() error {
+	if rabbit.close {
+		return errors.New("connection already close")
+	}
+
+	close(rabbit.done)
+
 	err := rabbit.channel.Close()
 	if err != nil {
 		return fmt.Errorf("error on closing channel: %w", err)
@@ -68,9 +87,60 @@ func (rabbit *Rabbit) CreateQueue(name string, maxRetries int) error {
 }
 
 func (rabbit *Rabbit) SendMessage(ctx context.Context, queue string, message any) error {
+	if rabbit.close {
+		return ErrConnectionClose
+	}
+
+	resendDelay := time.Second
+
+	for retries := 0; retries < rabbit.maxPublishRetries; retries++ {
+		chanConfirm, err := rabbit.sendMessage(ctx, queue, message)
+		if err != nil {
+			if errors.Is(err, ErrEncondingMessage) {
+				return err
+			}
+
+			select {
+			case <-rabbit.done:
+				return ErrConnectionClose
+			case <-time.After(resendDelay):
+				resendDelay *= 2
+			}
+
+			continue
+		}
+
+		ctx, cancel := context.WithTimeout(ctx, resendDelay)
+		defer cancel()
+
+		done, err := chanConfirm.WaitContext(ctx)
+		if err != nil {
+			continue
+		}
+
+		if done {
+			return nil
+		}
+	}
+
+	return ErrMaxRetries
+}
+
+func (rabbit *Rabbit) sendMessage(
+	ctx context.Context,
+	queue string,
+	message any,
+) (*amqp.DeferredConfirmation, error) {
+	if rabbit.close {
+		return nil, ErrConnectionClose
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	defer cancel()
+
 	messageEncoding, err := json.Marshal(message)
 	if err != nil {
-		return fmt.Errorf("error enconding message: %w", err)
+		return nil, errors.Join(ErrEncondingMessage, err)
 	}
 
 	publish := amqp.Publishing{
@@ -78,12 +148,19 @@ func (rabbit *Rabbit) SendMessage(ctx context.Context, queue string, message any
 		Body:        messageEncoding,
 	}
 
-	err = rabbit.channel.PublishWithContext(ctx, "", queue, false, false, publish)
+	confirm, err := rabbit.channel.PublishWithDeferredConfirmWithContext(
+		ctx,
+		"",
+		queue,
+		false,
+		false,
+		publish,
+	)
 	if err != nil {
-		return fmt.Errorf("error sending message: %w", err)
+		return nil, errors.Join(ErrSendingMessage, err)
 	}
 
-	return nil
+	return confirm, err
 }
 
 func New(config Config) (*Rabbit, error) {
@@ -109,7 +186,11 @@ func New(config Config) (*Rabbit, error) {
 	}
 
 	return &Rabbit{
-		connection: rabbit,
-		channel:    channel,
+		done:              make(chan bool),
+		close:             false,
+		maxPublishRetries: 5,
+		connection:        rabbit,
+		channel:           channel,
+		notifyClose:       make(chan amqp.Error),
 	}, nil
 }
