29844919bb5e01aee87b775a7d01f0602917e664
Thiago Felipe <thiago.felipe@tutanota.com>
2023-04-30 01:42:50 -0300

    Creating new models

--- publisher/model/model.go
+++ publisher/model/model.go
@@ -6,25 +6,11 @@ import (
 	"github.com/google/uuid"
 )
 
-type Receiver struct {
-	Name  string `json:"name"  bson:"name"  validate:"required"`
-	Email string `json:"email" bson:"email" validate:"required,email"`
-}
-
-type TemplateData struct {
-	Name string            `json:"name" bson:"name" validate:"required"`
-	Data map[string]string `json:"data" bson:"data"`
-}
-
-//nolint:lll
-type Email struct {
-	ID             uuid.UUID     `json:"-"              bson:"_id"             validate:"-"`
-	Receivers      []Receiver    `json:"receivers"      bson:"receivers"       validate:"required_without=BlindReceivers,omitempty,min=1"`
-	BlindReceivers []Receiver    `json:"blindReceivers" bson:"blind_receivers" validate:"required_without=Receivers,omitempty,min=1"`
-	Subject        string        `json:"subject"        bson:"subject"         validate:"required"`
-	Message        string        `json:"message"        bson:"message"         validate:"required_without=Template,excluded_with=Template"`
-	Template       *TemplateData `json:"template"       bson:"template"        validate:"required_without=Message,excluded_with=Message"`
-	Attachments    []string      `json:"attachments"    bson:"attachments"`
+type User struct {
+	ID       uuid.UUID `json:"-"        bson:"_id"      validate:"-"`
+	Name     string    `json:"name"     bson:"name"     validate:"required"`
+	Email    string    `json:"email"    bson:"email"    validate:"required,email"`
+	Password string    `json:"password" bson:"password" validate:"required"`
 }
 
 type QueuePartial struct {
@@ -40,6 +26,36 @@ type Queue struct {
 	CreatedAt  time.Time `json:"createdAt"  bson:"created_at"`
 }
 
+type Receiver struct {
+	Name  string `json:"name"  bson:"name"  validate:"required"`
+	Email string `json:"email" bson:"email" validate:"required,email"`
+}
+
+type TemplateData struct {
+	Name string            `json:"name" bson:"name" validate:"required"`
+	Data map[string]string `json:"data" bson:"data"`
+}
+
+//nolint:lll
+type Email struct {
+	ID             uuid.UUID     `json:"-"              bson:"_id"             validate:"-"`
+	UserID         uuid.UUID     `json:"-"              bson:"user_id"         validate:"-"`
+	Receivers      []Receiver    `json:"receivers"      bson:"receivers"       validate:"required_without=BlindReceivers,omitempty,min=1"`
+	BlindReceivers []Receiver    `json:"blindReceivers" bson:"blind_receivers" validate:"required_without=Receivers,omitempty,min=1"`
+	Subject        string        `json:"subject"        bson:"subject"         validate:"required"`
+	Message        string        `json:"message"        bson:"message"         validate:"required_without=Template,excluded_with=Template"`
+	Template       *TemplateData `json:"template"       bson:"template"        validate:"required_without=Message,excluded_with=Message"`
+	Attachments    []string      `json:"attachments"    bson:"attachments"`
+}
+
+type EmailList struct {
+	ID          uuid.UUID `json:"-"           bson:"_id"         validate:"-"`
+	UserID      uuid.UUID `json:"-"           bson:"user_id"     validate:"-"`
+	Emails      []string  `json:"emails"      bson:"emails"      validate:"required,dive,email,required"`
+	Name        string    `json:"name"        bson:"name"        validate:"required"`
+	Description string    `json:"description" bson:"description" validate:"required"`
+}
+
 type TemplatePartial struct {
 	Name     string `json:"name"     validate:"required"`
 	Template string `json:"template" validate:"required"`
@@ -47,7 +63,19 @@ type TemplatePartial struct {
 
 type Template struct {
 	ID       uuid.UUID `json:"-"        bson:"_id"`
+	UserID   uuid.UUID `json:"-"        bson:"user_id"  validate:"-"`
 	Name     string    `json:"name"     bson:"name"`
 	Template string    `json:"template" bson:"template"`
 	Fields   []string  `json:"fields"   bson:"fields"`
+}
+
+type Attachment struct {
+	ID     uuid.UUID `json:"-"    bson:"_id"     validate:"-"`
+	UserID uuid.UUID `json:"-"    bson:"user_id" validate:"-"`
+	Name   string    `json:"name" bson:"name"    validate:"required"`
+}
+
+type AttachmentLink struct {
+	Name string `json:"name"`
+	Link string `json:"link"`
 }
